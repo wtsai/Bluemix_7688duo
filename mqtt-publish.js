56c56732af117f445fff12456a1d0bc1074398a9
Wesley Tsai <wesleyboy42@gmail.com>
2016-03-22 00:38:08 +0800

    mqtt publish for IBM bluemix.

---
+++ mqtt-publish.js
@@ -0,0 +1,15 @@
+﻿var mqtt = require('mqtt');
+
+var clientId = ['d', "{網域}", "{裝置類型}", "{裝置ID}"].join(':');
+var iot_client = mqtt.connect("mqtt://{網域}.messaging.internetofthings.ibmcloud.com:1883",
+					  {
+						  "clientId" : clientId,
+						  "keepalive" : 30,
+						  "username" : "use-token-auth",
+						  "password" : "{裝置記號Token}"
+					  });
+
+iot_client.on('connect', function() {
+	console.log('7688 Duo client connected to IBM IoT Cloud.');
+	iot_client.publish('iot-2/evt/status/fmt/json', '{"d":{"status": "connected" }}');
+});
