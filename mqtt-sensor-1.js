e4adc4f712472ffd1000ba773f222521e6cd0c14
Wesley Tsai <wesleyboy42@gmail.com>
2016-04-17 23:12:37 +0800

    Add the sensor and the subscriber files for the filter example.

---
+++ mqtt-sensor-1.js
@@ -0,0 +1,16 @@
+﻿var mqtt = require('mqtt');
+
+var clientId = ['d', "{組織ID}", "{裝置類型}", "{裝置ID}"].join(':');
+var iot_client = mqtt.connect("mqtt://{組織ID}.messaging.internetofthings.ibmcloud.com:1883",
+					  {
+						  "clientId" : clientId,
+						  "keepalive" : 30,
+						  "username" : "use-token-auth",
+						  "password" : "{鑑別記號}"
+					  });
+
+iot_client.on('connect', function() {
+	console.log('7688 Duo sensor connected to IBM IoT Cloud.');
+	iot_client.publish('iot-2/evt/status/fmt/json', '{"d":{"status": "connected" }}');
+	iot_client.publish('iot-2/evt/Temperature/fmt/sting', '-10');
+});
---
+++ mqtt-subscriber-2.js
@@ -0,0 +1,19 @@
+﻿var mqtt = require('mqtt');
+
+var clientId = ['d', "{組織ID}", "{裝置類型}", "{裝置ID}"].join(':');
+var iot_client = mqtt.connect("mqtt://{組織ID}.messaging.internetofthings.ibmcloud.com:1883",
+					  {
+						  "clientId" : clientId,
+						  "keepalive" : 30,
+						  "username" : "use-token-auth",
+						  "password" : "{鑑別記號}"
+					  });
+
+iot_client.on('connect', function() {
+	console.log('7688 Duo subscriber connected to IBM IoT Cloud.');
+	iot_client.subscribe('iot-2/cmd/+/fmt/+');
+});
+
+iot_client.on("message", function(topic,payload){
+	console.log('received topic:'+topic+', payload:'+payload);
+});
